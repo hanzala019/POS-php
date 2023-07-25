933ca95bc2af89221daf302b1359892d704a3b6f
AdnanJami <muntasiradnanj@gmail.com>
2023-07-25 23:46:53 +0600

    Layout

--- POS-php/Client/Front2/buy.php
+++ POS-php/Client/Front2/buy.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style><?php include "style.css" ?> </style>
 </head>
 <body>
-    <div>
-        helo
-    </div>
-    <div class="diver"></div>
+    <div class="diver"  >Fahal</div>
+    <div class="downw">nigga</div>
+    <div class="rig">SUP</div>
+
 </body>
 </html>
--- POS-php/Client/index.php
+++ POS-php/Client/index.php
@@ -1,5 +1,3 @@
 <?php
 include './Front2/buy.php';
-include './components-h/header.php';
-echo "hello uncle Jami!";
 ?>
