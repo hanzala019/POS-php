224420a584b3a5210d646d53c8d83ccf4a12a595
hanzala <hasanbinomar.0.1.2.3@gmail.com>
2023-07-26 22:23:53 +0600

    comited

--- POS-php/Client/components-h/header.php
+++ POS-php/Client/components-h/header.php
@@ -1,3 +1,35 @@
-<div style="background-color: #333;">
-  <h1 style="color: #fafafa">LOGO</h1>  
+
+<head>
+<style>
+  <?php include "style.css"; ?>
+ </style>
+</head>
+
+<body class="bg">
+<div class="nav">
+  <div class="logo">
+    <div>
+      <h1>LOGO</h1>
+    </div>
+  </div>
+
+  <div class="link">
+      <div>
+        <h1> <a href="POS/POS-php/Client/components-h/dashboard.php"> Dashboard</a></h1>
+      </div>
+      <div>
+        <h1> Staff</h1>
+      </div>
+      <div>
+        <h1> Sales</h1>
+      </div>
+  </div>
+
+  <div class="login">
+    <div>
+      <h1>Login</h1>
+    </div>
+  </div>
 </div>
+  
+  
